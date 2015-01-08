fbdf7e19026b1df6a440e7afbf9ccacdbedc5291
tigerh <tigerrun1998@gmail.com>
2015-01-08 00:26:41 -0500

    Fixed elevator buttons.

--- src/com/techhounds/robot/RobotMap.java
+++ src/com/techhounds/robot/RobotMap.java
@@ -14,12 +14,12 @@ public class RobotMap {
     public static final int DRIVER_GAMEPAD = 1;
     static int HOME_MODULES = 6;
     
-    static int ELEVATOR_UP = 1;
+    static int ELEVATOR_UP = 4;
     static int ELEVATOR_DOWN = 2;
     
     public static final int elevatorMotor = 9;
-    public static final int elevatorBottomSwitch = 5;
-    public static final int elevatorTopSwitch = 6;
+    public static final int elevatorBottomSwitch = 7;
+    public static final int elevatorTopSwitch = 8;
     
     public static final double elevatorUpSpeed = 1.0;
     public static final double elevatorDownSpeed = 1.0;
